7c0a1d84748a3b3ea2bc9915fd9b4212972179f4
Paolagordillo <yenny.gordillo@est.iudigital.edu.co>
2025-03-03 01:45:49 +0000

    Prueba final

--- src/pad/Actividad_1.py
+++ src/pad/Actividad_1.py
@@ -31,7 +31,7 @@ Ingestion = Actividad_1()
 datos_json = Ingestion.leer_api("https://api.thecatapi.com/v1/images/search?limit=10")
 
 # Opción 1: Guardar en una ruta personalizada
-ruta_personalizada = "src/pad/static/json/actividad_2.json"
+ruta_personalizada = "src/pad/static/json/actividad_1ok.json"
 Ingestion.escribir_json(ruta_personalizada, datos_json)
 print(f"Datos guardados en: {ruta_personalizada}")
 
