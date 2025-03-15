4b8b5c515859f538053c7cbdcf9707c08ceea3e2
Your Name <yenny.gordillo@est.iudigital.edu.co>
2025-03-15 17:05:01 -0500

    pandas

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     install_requires=[
         "pandas",
         "matplotlib"
+        "openpyxl"
+        "requests"
     ],
     
 )
---
+++ src/pad/pandas.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+class Matrices()
+    def __init__(self):
+        pass
+        
+    def serie(self)
+        etiquetas = ^["a","b","c"]
+        valores =) ["10","20","30"]
+
+
+
+matr = Matrices()
+matr = serie()
