8f20fb73adc35849dc6b3016ca5d5ede8655f136
Your Name <yenny.gordillo@est.iudigital.edu.co>
2025-03-15 17:48:25 -0500

    actualizacion

--- setup.py
+++ setup.py
@@ -9,9 +9,12 @@ setup(
     description="configuracion pad",
     py_modules=["actividad_1"],
     install_requires=[
+          "kagglehub[pandas-datasets]>=0.3.8",
+        "matplotlib>=3.5.0",
+        "seaborn>=0.11.2",
         "pandas",
-        "matplotlib"
-        "openpyxl"
+        "numpy",
+        "openpyxl",
         "requests"
     ],
     
